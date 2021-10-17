3d94c0f737c9d39aa572a316ecf90353896c5fdd
shubhamchhimpa <chhimpa.shubh04@gmail.com>
2021-10-17 12:26:20 +0530

    added doc comments to trie file

--- src/utils/trie.js
+++ src/utils/trie.js
@@ -1,4 +1,10 @@
+// Author : Shubham Chhimpa
+
+/** Class representing a TrieNode. */
 class TrieNode {
+  /**
+   * Create a TrieNode.
+   */
   constructor() {
     this.children = new Array(26);
     for (let i = 0; i < 26; i++) {
@@ -8,13 +14,24 @@ class TrieNode {
   }
 }
 
+/** Class representing a Trie. */
 class Trie {
+  /**
+   * Create a Trie.
+   */
   constructor() {
+    //initializing root node
     this.root = new TrieNode();
   }
 
+  /**
+   * Add a keyword into the trie.
+   * @param {string} word - The keyword to be inserted.
+   */
   insert(word) {
-    word = word.toLowerCase();    
+    // converting the word to lowercase english letter
+    // since this trie class support only lowercase english letters
+    word = word.toLowerCase();
     let curr = this.root;
     let wordLen = word.length;
     for (let i = 0; i < wordLen; i++) {
@@ -29,42 +46,60 @@ class Trie {
     curr.isEnd = true;
   }
 
+
+  /**
+   * Search all keywords that matches the given keyword.
+   * @param {string} word - The keyword for matching.
+   * @return {Array} suggestions - Array of all the matching keywords
+   */
   search(word) {
-    word = word.toLowerCase();    
+    // converting the word to lowercase english letter
+    // since this trie class support only lowercase english letters
+    word = word.toLowerCase();
     let curr = this.root;
     let wordLen = word.length;
     let suggestions = [];
-    if (word.length ===0) return suggestions;
+    if (word.length === 0) return suggestions;
     for (let i = 0; i < wordLen; i++) {
-      let index = parseInt(word.charCodeAt(i)) - parseInt('a'.charCodeAt(0));
+      let index = parseInt(word.charCodeAt(i)) - parseInt("a".charCodeAt(0));
       if (curr.children[index] !== null) {
         curr = curr.children[index];
       } else {
-          if(i!==0){
-            return this.search(word.slice(0,i));
-          }
-          return suggestions;
+        if (i !== 0) {
+          return this.search(word.slice(0, i));
+        }
+        return suggestions;
       }
     }
 
-    this.searchRecursive(curr,suggestions,word)
+    this.searchRecursive(curr, suggestions, word);
     return suggestions;
-
   }
 
-  searchRecursive(curr,suggestions,word){
-    if(curr === null){
-        return;
+
+  /**
+   * Recursive healper function for search function.
+   * @param {TrieNode} curr - Trie node to be processed
+   * @param {Array} suggestions - Array of all the matching keywords
+   * @param {string} word - The prefix for matching.
+   */
+  searchRecursive(curr, suggestions, word) {
+    if (curr === null) {
+      return;
     }
 
-    if(curr.isEnd){
-        suggestions.push(word);
+    if (curr.isEnd) {
+      suggestions.push(word);
     }
 
     let children = curr.children;
 
-    for(let i =0;i<children.length;i++){
-        this.searchRecursive(children[i],suggestions,word+String.fromCharCode(i+parseInt('a'.charCodeAt(0))));
+    for (let i = 0; i < children.length; i++) {
+      this.searchRecursive(
+        children[i],
+        suggestions,
+        word + String.fromCharCode(i + parseInt("a".charCodeAt(0)))
+      );
     }
   }
 }
